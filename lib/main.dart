7d35fe9bc64893ec7af7d686ed99c3029c3b9902
jonathanmtzgzz <jonathanmtzgzz@hotmail.com>
2024-07-04 22:02:31 -0600

    Move chat_screen.dart to screens folder and update imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:talking_app/config/theme/app_theme.dart';
-import 'package:talking_app/presentation/chat/chat_screen.dart';
+import 'package:talking_app/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
